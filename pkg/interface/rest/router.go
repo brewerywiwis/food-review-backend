51f5d96a55084843044ad465a3570dc6a99f75f6
brewerywiwis <brewder136@hotmail.com>
2022-08-15 10:05:27 +0700

    add Server interface to use as rest server port

--- pkg/interface/rest/router.go
+++ pkg/interface/rest/router.go
@@ -2,12 +2,14 @@ package rest
 
 import "github.com/gin-gonic/gin"
 
-func registerRoutes(server *gin.Engine) {
-	api := server.Group("/api")
-
-	v1 := api.Group("/v1")
-	v1.GET("/healthcheck", func(ctx *gin.Context) {
+func registerV1Route(router *gin.RouterGroup) {
+	router.GET("/healthcheck", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{"msg": "success"})
 	})
+}
 
+func registerRoutes(server *GinServer) {
+	api := server.engine.Group("/api")
+	v1 := api.Group("/v1")
+	registerV1Route(v1)
 }
--- pkg/interface/rest/server.go
+++ pkg/interface/rest/server.go
@@ -1,9 +1,27 @@
 package rest
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
-func NewServer() *gin.Engine {
-	server := gin.Default()
+// Server represents rest server interface
+type Server interface {
+	Run(addr string) error
+}
+
+// GinServer is implementation of rest server by Gin
+type GinServer struct {
+	engine *gin.Engine
+}
+
+// Run is used to start a gin server
+func (s *GinServer) Run(addr string) error {
+	return s.engine.Run(addr)
+}
+
+// NewServer returns a new Server
+func NewServer() Server {
+	server := &GinServer{gin.Default()}
 
 	registerRoutes(server)
 
