8b6efed0ccd6816e7ca282bc5ee3290d3b1c90f9
brewerywiwis <brewder136@hotmail.com>
2022-08-29 17:18:41 +0700

    Updates: add go swagger 1.add gin-swagger to create a document 2.fix air.toml to exclude docs directory

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,15 +1,40 @@
 package main
 
 import (
+	"fmt"
+
+	docs "github.com/brewerywiwis/food-review-backend/docs"
 	"github.com/brewerywiwis/food-review-backend/pkg/domain/food"
 	"github.com/brewerywiwis/food-review-backend/pkg/infrastructure/memory"
 	"github.com/brewerywiwis/food-review-backend/pkg/interface/rest"
 )
 
+// @title Food Review System API
+// @version 0.1.0
+// @description This is a food review system api.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name brewerywiwis
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @schemes http
 func main() {
 	foodRepository := memory.FoodStorage{}
 	foodService := food.NewService(&foodRepository)
 	server := rest.NewServer(foodService)
 
-	server.Run(":8090")
+	// prepare to make a config env
+	port := 8090
+	basePath := "/"
+	appVersion := "0.1.0"
+
+	docs.SwaggerInfo.Host = fmt.Sprintf("http://localhost:%d", port)
+	docs.SwaggerInfo.BasePath = basePath
+	docs.SwaggerInfo.Version = appVersion
+
+	server.Run(fmt.Sprintf(":%d", port))
 }
--- pkg/interface/rest/router.go
+++ pkg/interface/rest/router.go
@@ -3,6 +3,8 @@ package rest
 import (
 	"github.com/brewerywiwis/food-review-backend/pkg/domain/food"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func registerV1Routes(router *gin.RouterGroup, foodService food.Service) {
@@ -13,6 +15,11 @@ func registerV1Routes(router *gin.RouterGroup, foodService food.Service) {
 	foodController := foodController{foodService}
 	router.GET("/foods", foodController.listAllFood)
 	router.POST("/food", foodController.createFood)
+
+	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler,
+		ginSwagger.DefaultModelsExpandDepth(-1),
+		ginSwagger.PersistAuthorization(true)),
+	)
 }
 
 func registerRoutes(server *GinServer, foodService food.Service) {
